03406e551e752a81afb0ba69be9e89a30e35ee67
fundwotsai2001 <64879498+fundwotsai2001@users.noreply.github.com>
2023-06-10 03:10:53 +0800

    Add files via upload

--- utils/pcd_utils.py
+++ utils/pcd_utils.py
@@ -1,6 +1,7 @@
 import open3d as o3d
 from typing import List
 from tqdm import tqdm
+import os
 def numpy2pcd(arr):
     # turn numpy array into open3d point cloud format
     pcd = o3d.geometry.PointCloud()
@@ -10,53 +11,48 @@ def numpy2pcd(arr):
 def savepcd(filename, pcd):
     o3d.io.write_point_cloud(filename, pcd)
 
-def visualize_pcds(pcd_list: List) -> None:
+from typing import List
+import open3d as o3d
+import os
+
+from typing import List
+import open3d as o3d
+import numpy as np
+import os
+
+from typing import List
+import open3d as o3d
+import numpy as np
+import os
+
+def visualize_pcds(pcd_list: List[o3d.geometry.PointCloud], target_pcd: o3d.geometry.PointCloud) -> None:
     print("""
     ---------------------------------------------------
     Visualizing the input point clouds for debugging...
-    press SPACE would change to the next point cloud.
-    press B would change to the previouw point cloud. 
     ---------------------------------------------------\n""")
-    g_idx = 0
-    vis = o3d.visualization.VisualizerWithKeyCallback()
-    pbar = tqdm(total = len(pcd_list))
-
-    def show_pointcloud(vis):
-        nonlocal g_idx
-        pcd = pcd_list[g_idx]
-        # pcd = o3d.geometry.PointCloud()
-        # pcd.points = o3d.utility.Vector3dVector(points)
-        vis.clear_geometries()
-        vis.add_geometry(pcd) # fuck bug, vis.update_geometry(pcd)没有用！
-        vis.update_renderer()
-        vis.poll_events()
-
-    def key_forward_callback(vis):
-        nonlocal g_idx
-        g_idx += 1
-        if g_idx >= len(pcd_list):
-            g_idx = len(pcd_list) - 1
-        show_pointcloud(vis)
-        # print(f"{g_idx / len(pcd_list):.3f}%", end='\r')
-        pbar.update(1)
-        return True
-
-    def key_back_callback(vis):
-        nonlocal g_idx
-        g_idx -= 1
-        if g_idx < 0:
-            g_idx = 0
-        show_pointcloud(vis)
-        # print(f"{g_idx / len(pcd_list):.3f}%", end='\r')
-        pbar.update(-2)
-        pbar.update(1)
-        return True
-
-    vis.create_window()
-    vis.get_render_option().point_size = 0.2  # set points size
-
-    vis.register_key_callback(ord(' '), key_forward_callback)  # space
-    vis.register_key_callback(ord('B'), key_back_callback)  # fuck bug, 字母必须是大写!
-    vis.run()
-
     
+    output_folder = 'output_images/'
+    os.makedirs(output_folder, exist_ok=True)
+    
+    # Set a color for the target point cloud
+    target_pcd.paint_uniform_color([0, 1, 0])  # green for the target pcd
+
+    for i, pcd in enumerate(pcd_list):
+        if i%10 == 1:
+            filename = f"{output_folder}pointcloud_{i}.png"
+            
+            # Set a color for the current point cloud
+            pcd.paint_uniform_color([1, 0, 0])  # red for the first pcd
+
+            vis = o3d.visualization.Visualizer()
+            vis.create_window(visible=False)
+            vis.add_geometry(pcd)
+            vis.add_geometry(target_pcd)  # add the target point cloud to every visualization
+            vis.update_renderer()
+            vis.capture_screen_image(filename, do_render=True)
+            vis.destroy_window()
+            
+            print(f"Saved image: {filename}")
+
+
+
